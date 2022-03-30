b7c66576a79cbef394dc6e2e46a54f63a3ec1f7b
saurabh rajput <saurabhkgf2609@gmail.com>
2022-03-30 08:50:12 +0530

    loops

---
+++ Loops.java
@@ -0,0 +1,16 @@
+public class Loops {
+    public static void main(String[] args) {
+        System.out.println("Loops...");
+        System.out.println("for loop...");
+        // for( int i = 0; i<10; i++){
+        // System.out.println("hello");
+        // }
+
+        // System.out.println("While loop...");
+        // int i = 0;
+        // while (i < 10) {
+        //     System.out.println("heoool");
+        //     i++;
+        // }
+    }
+}
