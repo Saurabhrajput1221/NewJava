78704df7b45271e350cdb94e0365eabfc95e19c1
saurabh rajput <saurabhkgf2609@gmail.com>
2022-03-31 16:10:37 +0530

    class...

---
+++ MyJava/Array.java
@@ -0,0 +1,30 @@
+public class Array {
+    public static void main(String[] args) {
+         // Array...
+    
+    int[] arr = {1,2,3,4,5,6};
+    //   System.out.println(arr.length);
+        for( int i=0; i<arr.length; i++){
+            arr[i] = arr[i] * arr[i*1];
+               System.out.println(arr[i]);
+        }
+       
+       
+    // // modify array...
+    //  arr[2] = 10;
+    //  System.out.println(arr[2]);
+    //   System.out.println(arr.length);
+      
+    // //   System.out.println(arr);
+    // // how to print whole array...
+    // for (int i = 0; i<arr.length; i++){
+    //      System.out.print(arr[i] + " ");
+    // }
+    
+    // int[] arr2 = new int[10];
+    // for(int i =0; i<arr2.length; i++){
+        
+    //   System.out.print(arr2[i]);
+    // }
+    }
+}
