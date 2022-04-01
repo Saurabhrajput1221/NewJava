69d3df42d9de28e2b43c66f2f6dc363ab6b4ad8a
saurabh rajput <saurabhkgf2609@gmail.com>
2022-04-01 13:30:26 +0530

    practice.

--- Code_With_Harry/ArraysQuestions.java
+++ Code_With_Harry/ArraysQuestions.java
@@ -20,38 +20,49 @@ public class ArraysQuestions {
         // }
         // System.out.println(sum/(marks.length));
 
-        // // Question no3...(i didnot write at my own...) Create matrix [2][2] print sum
+        // // Question no3...(i didnot write at my own...) Create matrix [2][2] print
+        // sum
         // // of matrix...
 
         // int[][] mat1 = {
-        //         { 1, 2, 3 },
-        //         { 4, 5, 6 }
+        // { 1, 2, 3 },
+        // { 4, 5, 6 }
         // };
         // int[][] mat2 = {
-        //         { 5, 8, 3 },
-        //         { 4, 6, 6 }
+        // { 5, 8, 3 },
+        // { 4, 6, 6 }
         // };
         // int[][] result = {
-        //         { 0, 0, 0 },
-        //         { 0, 0, 0 }
+        // { 0, 0, 0 },
+        // { 0, 0, 0 }
         // };
 
         // for (int i = 0; i < mat1.length; i++) { // row number of times...
-        //     for (int j = 0; j < mat1[i].length; j++) { // column number of times...
-        //         System.out.print(result[i][j] + " ");
-        //         result[i][j] = mat1[i][j] + mat2[i][j];
-        //     }
-        //     System.out.println(""); // print new line only...
+        // for (int j = 0; j < mat1[i].length; j++) { // column number of times...
+        // System.out.print(result[i][j] + " ");
+        // result[i][j] = mat1[i][j] + mat2[i][j];
+        // }
+        // System.out.println(""); // print new line only...
         // }
 
+        // Question no4...(Note: this question is very very important...) print Reverse
+        // Array...
 
-         // Question no4... print Reverse Array...
+        // int[] rollno = {1,2,3,4,5,6};
+        // for(int i= rollno.length; i>0; i--){
+        // System.out.print(rollno[i]);
 
-         int[] rollno = {1,2,3,4,5,6};
-         for(int i= rollno.length; i>0; i--){
-             System.out.print(rollno[i]);
-         }
-   
+        // }
+
+        // Question no5... print max number of array...
+        int[] arr = { 1, 3, 65675, 7, 8 };
+        int max = arr[0];
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] > max)
+                max = arr[i];
+
+        }
+        System.out.print(max);
 
     }
 }
