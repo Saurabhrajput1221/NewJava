318c1c550c970a3a4a97e9383d420520f34593e8
saurabh rajput <saurabhkgf2609@gmail.com>
2022-04-07 23:51:49 +0530

    a

--- Code_With_Harry/Custom_class.java
+++ Code_With_Harry/Custom_class.java
@@ -1,30 +1,43 @@
-class Employee{
+// You need more and more Practice 
+class Employee {
     int id;
     String name;
-    public void printDetails(){
+    int salary;
+    String Info;
+
+    public void printDetails() {
         System.out.println("My id is " + id);
         System.out.println("and my name is " + name);
     }
-    public int getsalary(){
-        
+
+    public int getsalary() {
+
         return salary;
-         
+
+    }
+    public String Information(){
+        return Info;
     }
 
 }
+
 public class Custom_class {
     public static void main(String[] args) {
         // pls do Practice more...
-        Employee harry = new Employee();
+        Employee harry = new Employee(); // Instantiating a new Employee object
 
+        // Setting Attributes...
         harry.id = 55;
         harry.name = "saurabh";
-        harry.salary = 56000;
-
         harry.printDetails();
-        harry.getsalary();
+        
+        harry.salary = 56000;
+        int abc = harry.getsalary();
+        System.out.println(abc);
 
+        harry.Info = "He is having BMW and own plane....";
+        String moreInfo = harry.Info;
+        System.out.println(moreInfo);
 
     }
 }
-
