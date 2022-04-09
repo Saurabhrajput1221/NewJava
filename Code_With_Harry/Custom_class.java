4c843945c8b34e13189489e0546f09e157e06f3f
saurabh rajput <saurabhkgf2609@gmail.com>
2022-04-09 09:53:31 +0530

    custom class practice

--- Code_With_Harry/Custom_class.java
+++ Code_With_Harry/Custom_class.java
@@ -1,43 +1,81 @@
-// You need more and more Practice 
-class Employee {
+// I did more practice on Custom class
+class saurabh {
     int id;
-    String name;
+    String Lname;
     int salary;
-    String Info;
 
-    public void printDetails() {
-        System.out.println("My id is " + id);
-        System.out.println("and my name is " + name);
+    public void Details() {
+        System.out.println(" My surname  is " + Lname);
+        System.out.println(" and my id is " + id);
     }
+    public int MoreDetails(){
+        return salary ;
 
-    public int getsalary() {
-
-        return salary;
-
-    }
-    public String Information(){
-        return Info;
     }
 
 }
-
 public class Custom_class {
     public static void main(String[] args) {
-        // pls do Practice more...
-        Employee harry = new Employee(); // Instantiating a new Employee object
+        System.out.println("My practice Custom class ");
+        saurabh abc = new saurabh();
 
-        // Setting Attributes...
-        harry.id = 55;
-        harry.name = "saurabh";
-        harry.printDetails();
-        
-        harry.salary = 56000;
-        int abc = harry.getsalary();
-        System.out.println(abc);
+        abc.id = 45;
+        abc.Lname = "Rajput";
 
-        harry.Info = "He is having BMW and own plane....";
-        String moreInfo = harry.Info;
-        System.out.println(moreInfo);
+        System.out.println(abc.id);
+        System.out.println(abc.Lname);
+
+        abc.Details();
+
+        abc.salary = 56000;
+        int salary = abc.MoreDetails() ;
+        System.out.println(salary);
 
     }
 }
+
+
+
+// // You need more and more Practice 
+// class Employee {
+//     int id;
+//     String name;
+//     int salary;
+//     String Info;
+
+//     public void printDetails() {
+//         System.out.println("My id is " + id);
+//         System.out.println("and my name is " + name);
+//     }
+
+//     public int getsalary() {
+
+//         return salary;
+
+//     }
+//     public String Information(){
+//         return Info;
+//     }
+
+// }
+
+// public class Custom_class {
+//     public static void main(String[] args) {
+//         // pls do Practice more...
+//         Employee harry = new Employee(); // Instantiating a new Employee object
+
+//         // Setting Attributes...
+//         harry.id = 55;
+//         harry.name = "saurabh";
+//         harry.printDetails();
+
+//         harry.salary = 56000;
+//         int abc = harry.getsalary();
+//         System.out.println(abc);
+
+//         harry.Info = "He is having BMW and own plane....";
+//         String moreInfo = harry.Info;
+//         System.out.println(moreInfo);
+
+//     }
+// }
