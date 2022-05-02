83a0744b05cffa1c6da7a4db75adab555af81b10
saurabh rajput <saurabhkgf2609@gmail.com>
2022-05-02 23:42:54 +0530

    A

---
+++ Code_With_Harry/AdvanceJava/HashingInjava.java
@@ -0,0 +1,24 @@
+package AdvanceJava;
+
+import java.util.HashSet;
+
+/**
+ * HashingInjava
+ */
+public class HashingInjava {
+
+    public static void main(String[] args) {
+        System.out.println("Hashing In java... ");
+        HashSet<Integer> MyHshset = new HashSet<>(6 , 05f);
+
+        MyHshset.add(1);
+        MyHshset.add(2);
+        MyHshset.add(3);
+        MyHshset.add(4);
+        MyHshset.add(5);
+         
+        System.out.println(MyHshset);
+        
+
+    }
+}
