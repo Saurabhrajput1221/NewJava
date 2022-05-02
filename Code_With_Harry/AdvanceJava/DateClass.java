771f7e00ffb906d9643694874ce9d8acc61bf50a
saurabh rajput <saurabhkgf2609@gmail.com>
2022-05-02 23:42:45 +0530

    A

---
+++ Code_With_Harry/AdvanceJava/DateClass.java
@@ -0,0 +1,15 @@
+package AdvanceJava;
+
+import java.util.Date;
+
+public class DateClass {
+    public static void main(String[] args) {
+        System.out.println("DateClass in java...");
+        // System.out.println(System.currentTimeMillis());
+        Date d = new  Date(); 
+       System.out.println(d); 
+
+       System.out.println(d.getTime());
+       System.out.println(d.getYear() - 1900);
+    }
+}
