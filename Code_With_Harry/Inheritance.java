5053355052e6696f70dad492ab374ef85a2d0b39
saurabh rajput <saurabhkgf2609@gmail.com>
2022-05-03 20:44:50 +0530

    A

--- Code_With_Harry/Inheritance.java
+++ Code_With_Harry/Inheritance.java
@@ -1,45 +1,95 @@
-// // package chpter10;
+// import Oops.Inheritance;
+// import java.lang.InstantiationError;
 
-// class Base {
-//     int x;
+class Base2 {
+    int x;
 
-//     public int getx() {
-//         return x;
-//     }
+    public int getx() {
+        return x;
+    }
 
-//     public void setx(int n) {
-//         x = n;
-//     }
+    public void setx(int n) {
+        x = n;
+    }
 
-//     public void printMe() {
-//         System.out.println("I am a cunstructor");
-//     }
+    public void printMe() {
+        System.out.println("I am a cunstructor");
+    }
+
+}
+
+class childbase extends Base2 {
+
+    int y;
+
+    public void sety(int n) {
+        y = n;
+    }
+
+    public int gety() {
+        return y;
+    }
+}
+
+public class Inheritance {
+    public static void main(String[] args) {
+        System.out.println("Inheritance");
+        // Inheritance Example of parents class
+        Base2 hi = new Base2();
+        hi.printMe();
+
+        // hi.setx(78);
+        // System.out.println(hi.getx());
+
+        // Inheritance Example of child class
+        // childbase ch = new childbase();
+        // ch.sety(87);
+        // System.out.println(ch.gety());
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// class Base2 {
+// int x;
+
+// public int getx() {
+// return x;
 
 // }
 
-// class childbase extends Base {
-
-//     int y ;
-//     public void sety(int n){
-//         y = n;
-//     }
-//     public int gety(){
-//         return y;
-//     }
+// public void setx(int n) {
+// x = n;
 // }
 
-// public class Inheritance {
-//     public static void main(String[] args) {
-//         System.out.println("Inheritance");
-//         // Inheritance Example of parents class
-//         Base hi = new Base();
-//         hi.printMe();
-//         hi.setx(78);
-//         System.out.println(hi.getx());
+// public void printme() {
+// System.out.println("I am constructor...");
+// }
+// }
 
-//         // Inheritance Example of child class
-//         childbase ch = new childbase();
-//         ch.sety(87);
-//         System.out.println(ch.gety());
-//     }
+// class childbase extends Base2 {
+// int y;
+
+// public void sety(int n) {
+// y = n;
+// }
+// public int gety(){
+// return y;
+// }
+// }
+
+// class babyderived {
+
 // }
