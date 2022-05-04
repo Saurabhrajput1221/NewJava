da1c70417f6f596822f4506bbea09f64f87ea681
saurabh rajput <saurabhkgf2609@gmail.com>
2022-05-04 10:54:18 +0530

    Aa

---
+++ DSA/Methods.java
@@ -0,0 +1,12 @@
+package DSA;
+
+public class Methods {
+   static int add(int a , int b){
+        return a+b;
+    }
+    public static void main(String[] args) {
+        System.out.println("Methods in java...");
+        // Without creating an object...  
+        System.out.print(add(2,3));
+    }
+}
