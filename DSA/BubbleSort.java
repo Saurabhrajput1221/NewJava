4f7f5c8a32c7a17fe05248b2e68263f0b9b94fd5
saurabh rajput <saurabhkgf2609@gmail.com>
2022-06-02 19:57:17 +0530

    changes

--- DSA/BubbleSort.java
+++ DSA/BubbleSort.java
@@ -1,17 +1,19 @@
 package DSA;
 
+import java.util.Scanner;
+
 import javax.print.DocFlavor.STRING;
 
 public class BubbleSort {
     // Below lines printing only array
     // Bubble sort of Numbers
-    static void PrintArr(int[] arr) {
+    static void PrintArr(int arr[]) {
         for (int i = 0; i < arr.length; i++) {
             System.out.print(arr[i] + " ");
         }
     }
 
-    // Bubble sort using String
+    // Bubble sort of using String
     // static void PrintArr(String[] helo) {
     // for (int i = 0; i < helo.length; i++) {
     // System.out.print(helo[i] + " ");
@@ -19,11 +21,52 @@ public class BubbleSort {
     // }
 
     public static void main(String[] args) {
-        System.out.println("Bubble Sort");
+        System.out.println(
+                "Bubble Sort using 1) Take input from user, 2) Bubble sort of Number, 3)Bubble sort of String");
 
+        // Scanner sc = new Scanner(System.in);
+
+        // /********************************************************* */
+        // // Take input from user....(code giving wrong output.....)
+        // System.out.print("Enter your array length and elements");
+        // int n = sc.nextInt();
+        // int arr[] = new int[n];
+        // for (int i = 0; i < n; i++) {
+        // arr[i] = sc.nextInt();
+        // }
+        // System.out.println("the array elements are");
+        // for (int i = 0; i < n; i++) {
+        // // System.out.print(arr[i] + " ");
+        // }
+        // for (int i = 0; i < arr.length -1; i++) {
+        // for (int j = 0; j < arr.length - 1 ; j++) {
+        // if (arr[j] > arr[j + 1]) {
+
+        // int temp = arr[j];
+        // arr[j] = arr[j + 1];
+        // arr[j + 1] = temp;
+
+        // }
+        // }
+        // System.out.print(arr[i] + " ");
+
+        // int f = arr[0];
+        // int L = arr[arr.length];
+        // System.out.println("first element is " + f);
+        // System.out.println("last element is " + L);
+        // }
+        // int k = n - 1;
+        // System.out.println("Array is sorted in " + k + " swap");
+
+        /********************************************************* */
+        // Bubble sort using of Numbers.....
+        // int n = sc.nextInt();
+        // int arr[i] = sc.nextInt();
+        // int m = sc.nextInt();
+        // int arr [] = new int[n];
         int arr[] = { 434, 546, 653, 3745, 646342, 45 };
 
-        // System.out.println(arr.length);
+        System.out.println(arr.length);
 
         for (int i = 0; i < arr.length; i++) {
             for (int j = 0; j < arr.length - 1 - i; j++) {
@@ -40,6 +83,7 @@ public class BubbleSort {
         PrintArr(arr);
         // time complexity is n^2
 
+        /********************************************************* */
         // Bubble sort using String
         // String helo[] = { "s", "b", "c", "d", "p", "a", "e", "f", "g" };
         // for (int i = 0; i < helo.length; i++) {
@@ -54,6 +98,8 @@ public class BubbleSort {
         // }
 
         // PrintArr(helo);
+
+        /********************************************************* */
 
         // // Swaping elements.....
         // int a = 33;
