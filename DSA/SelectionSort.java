b3cf574dabbf58c6f7d6d4bcf310afc2714ae833
saurabh rajput <saurabhkgf2609@gmail.com>
2022-06-03 09:58:16 +0530

    selection sort

--- DSA/SelectionSort.java
+++ DSA/SelectionSort.java
@@ -1,8 +1,33 @@
 package DSA;
 
 public class SelectionSort {
+    static void SelectionSortArr(int[] arr) {
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print(arr[i] + " ");
+        }
+    }
+
     public static void main(String[] args) {
         System.out.println("Slection Sort");
-        
+        int arr[] = { 7, 8, 3, 1, 2 };
+
+        for (int i = 0; i < arr.length; i++) {
+            int small = i;
+
+            for (int j = i + 1; j < arr.length; j++) {
+                if (arr[small] > arr[j]) {
+                    small = j;
+                    
+                    // System.out.println(arr[i] + " ");
+                }
+            }
+            // Swaping .....
+            int temp = arr[small];
+            arr[small] = arr[i];
+            arr[i] = temp;
+            // TimeComplexity n^2
+
+        }
+        SelectionSortArr(arr);
     }
 }
