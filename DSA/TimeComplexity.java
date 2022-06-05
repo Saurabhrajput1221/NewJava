d51e45a46c17426f6e2cb63d8e5698d03f5b30df
saurabh rajput <saurabhkgf2609@gmail.com>
2022-06-05 08:31:18 +0530

    changes

--- DSA/TimeComplexity.java
+++ DSA/TimeComplexity.java
@@ -1,21 +1,21 @@
-package DSA;
+// package DSA;
 
-import java.util.Scanner;
+// import java.util.Scanner;
 
-public class TimeComplexity {
-    public static void main(String[] args) {
-        System.out.println("Time Complexity");
+// public class TimeComplexity {
+//     public static void main(String[] args) {
+//         System.out.println("Time Complexity");
 
-        Scanner sc = new Scanner(System.in);
-        System.out.println("Enter Number");
-        int n =  sc.nextInt();
+//         Scanner sc = new Scanner(System.in);
+//         System.out.println("Enter Number");
+//         int n =  sc.nextInt();
 
-        for(int i =0; i<n; i++){
-            System.out.println("herdf");
-            for(int j =0; j<n; j++){
-                System.out.println("hello");
-            }
-        }
+//         for(int i =0; i<n; i++){
+//             System.out.println("herdf");
+//             for(int j =0; j<n; j++){
+//                 System.out.println("hello");
+//             }
+//         }
 
-    }
-}
+//     }
+// }
