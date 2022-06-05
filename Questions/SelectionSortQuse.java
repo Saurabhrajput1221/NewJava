7a4b64675d9cd3b7e8dc499b116323e53e29bc3c
saurabh rajput <saurabhkgf2609@gmail.com>
2022-06-05 08:30:44 +0530

    changes

--- Questions/SelectionSortQuse.java
+++ Questions/SelectionSortQuse.java
@@ -1,33 +1,33 @@
-package Questions;
+// package Questions;
 
-import java.util.Scanner;
+// import java.util.Scanner;
 
-public class SelectionSortQuse {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        // below code giving
-        int n = sc.nextInt();
-        int arr[] = new int[n];
+// public class SelectionSortQuse {
+//     public static void main(String[] args) {
+//         Scanner sc = new Scanner(System.in);
+//         // below code giving
+//         int n = sc.nextInt();
+//         int arr[] = new int[n];
 
-        for (int i = 0; i < arr.length; i++) {
-            arr[i] = sc.nextInt();
-            // System.out.println(arr[i] + " ");
-        }
+//         for (int i = 0; i < arr.length; i++) {
+//             arr[i] = sc.nextInt();
+//             // System.out.println(arr[i] + " ");
+//         }
 
-        for (int i = 0; i < arr.length; i++) {
-            int small = i;
-            for (int j = i + 1; i < arr.length; j++) {
-                if (arr[i] > j) {
-                    small = j;
-                }
-            }
-            int temp = arr[small];
-            arr[small] = arr[i];
-            arr[i] = temp;
-            System.out.println(arr[i] + " ");
-        }
-        // for (int i = 0; i < arr.length; i++) {
-        // System.out.println(arr[i] + " ");
-        // }
-    }
-}
+//         for (int i = 0; i < arr.length; i++) {
+//             int small = i;
+//             for (int j = i + 1; i < arr.length; j++) {
+//                 if (arr[i] > j) {
+//                     small = j;
+//                 }
+//             }
+//             int temp = arr[small];
+//             arr[small] = arr[i];
+//             arr[i] = temp;
+//             System.out.println(arr[i] + " ");
+//         }
+//         // for (int i = 0; i < arr.length; i++) {
+//         // System.out.println(arr[i] + " ");
+//         // }
+//     }
+// }
