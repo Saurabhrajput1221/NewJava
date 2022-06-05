5fd8905f43691428fbe1862880745cbe07c3976f
saurabh rajput <saurabhkgf2609@gmail.com>
2022-06-05 12:59:49 +0530

    practice

---
+++ Questions/TwoOddSum.java
@@ -0,0 +1,26 @@
+package Questions;
+
+import java.util.Scanner;
+
+public class TwoOddSum {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int SumOdd = 0;
+        int SumEven = 0;
+        int[]  arr = new int[n];
+
+        for(int i=0; i<arr.length; i++){
+            arr[i] = sc.nextInt();
+
+            if(arr[i] % 2 == 0){
+                SumEven = arr[i] + SumEven;
+                
+            }else{
+
+                SumOdd = arr[i] + SumOdd;
+            }
+        }
+        System.out.println(SumOdd);
+    }
+}
