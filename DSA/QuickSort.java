3f511955e6ad1c68d584b621df6cb99d40a1c43c
saurabh rajput <saurabhkgf2609@gmail.com>
2022-06-06 11:37:44 +0530

    pr

---
+++ DSA/QuickSort.java
@@ -0,0 +1,58 @@
+package DSA;
+
+public class QuickSort {
+    int partition(int[] arr, int low, int high) {
+
+        int pivot = (arr[low] + arr[high]) / 2;
+        while (low <= high) {
+            while (arr[low] < pivot) {
+                low++;
+                while (arr[high] > pivot) {
+                    high--;
+                }
+                if (low <= high) {
+                    int temp = arr[low];
+                    arr[low] = arr[high];
+                    arr[high] = temp;
+
+                    low++;
+                    high--;
+                }
+            }
+        }
+        return low;
+
+    }
+
+    public void QuickSortRecursion(int[] arr, int low, int high) {
+
+        int pi = partition(arr, low, high);
+        if (low < pi - 1) {
+            QuickSortRecursion(arr, low, pi - 1);
+        }
+        if (pi < high) {
+            QuickSortRecursion(arr, pi, high);
+        }
+    }
+
+    public void pritnarr(int arr[] , int l) {
+        for (int i = 0; i <l; i++) {
+            System.out.print(arr[i] + " ");
+        }
+    }
+
+    public static void main(String[] args) {
+        System.out.println("Quick Sort.....");
+        // Below code giving wrong output
+        int arr[] = { 6, 5, 4, 3, 2, 1, 5, 3, 5 };
+        int l = arr.length;
+        System.out.println(l);
+        QuickSort qsm = new QuickSort();
+        qsm.pritnarr(arr, l -1);
+
+        qsm.partition(arr, 0, arr.length-1);
+        qsm.pritnarr(arr , l);
+     System.out.println();
+
+    }
+}
---
+++ DSA/QuickSort2.java
@@ -0,0 +1,57 @@
+package DSA;
+
+public class QuickSort2 {
+    public static int partition(int arr[], int low, int high) {
+        // we set the pivot as last index of Array
+        int pivot = arr[high];
+        // i calculate how many empty spaces required for pivot Index
+        // we consider there is no small element compare to pivot
+        int i = low - 1;
+
+        // we are running loop for low to high index
+        for (int j = low; j < high; j++) {
+            // we are checkig following condition
+            if (arr[j] < pivot) {
+                // after checking condition we increase the index valve
+                i++;
+                // swap
+                int temp = arr[i];
+                arr[i] = arr[i];
+                arr[j] = temp;
+            }
+        }
+        i++;
+        int temp = arr[i];
+        arr[i] = pivot;
+        arr[high] = temp;
+        // i is our pivot index
+        return i;
+
+    }
+
+    public static void QuickSort(int arr[], int low, int high) {
+
+        if (low < high) {
+            // we are calculating extact position of pivot index
+            int pivotIdx = partition(arr, low, high);
+            // for left side Array (from pivot)
+            QuickSort(arr, low, pivotIdx - 1);
+            // for Right side Array (from pivot)
+            QuickSort(arr, pivotIdx + 1, high);
+
+        }
+    }
+
+    public static void main(String[] args) {
+        System.out.println("Quick Sort by Apana collage.....");
+        int arr[] = { 6, 5, 4, 3, 1, 46 };
+        int n = arr.length;
+        // we are calling our function / Method
+        QuickSort(arr, 0, n - 1);
+
+        for (int i = 0; i < n; i++) {
+            System.out.print(arr[i] + " ");
+        }
+
+    }
+}
