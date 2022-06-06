447e8991f4b2bef77269fe58eae3cdefd2abbe1a
saurabh rajput <saurabhkgf2609@gmail.com>
2022-06-06 11:38:06 +0530

    pr

---
+++ DSA/LinkList.java
@@ -0,0 +1,38 @@
+package DSA;
+
+import java.util.HexFormat;
+
+
+// import java.util.LinkedList;
+
+public class LinkList {
+    class Node{
+        String data;
+        Node next;
+        Node(String data){
+            this.data = data;
+            this.next = null;
+        
+        }
+    }
+    // add - first and Last
+    public void addfrist(String data){
+        Node newNode = new Node(data);
+        if(head == null){
+            head = newNode;
+            return;
+        }
+        newNode.next = head;
+        head = newNode;
+    }
+
+    public static void main(String[] args) {
+        System.out.println("LinkedList..... of DSA");
+        // LinkedList<Integer> l1 = new LinkedList<>();
+        // l1.add(43543);
+        // System.out.println(l1);
+
+        LinkList list = new LinkList();
+        // list.add("fj");
+    }
+}
