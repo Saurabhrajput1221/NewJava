82fa4d7c2a651001856a871cfbb45765f89e189a
saurabh rajput <saurabhkgf2609@gmail.com>
2022-06-10 23:49:23 +0530

    pr

---
+++ Questions/RectPattern.java
@@ -0,0 +1,68 @@
+package Questions;
+
+import java.util.Scanner;
+import java.util.regex.Pattern;
+
+public class RectPattern {
+    public static void main(String[] args) {
+        System.out.println("Rectangular pattern");
+        // Scanner sc = new Scanner(System.in);
+        // int n = sc.nextInt();
+        // // int k = sc.nextInt();
+        // Rectangular pattern
+        // for(int i =0; i<n; i++){
+        // for(int j =0; j<k; j++){
+        // System.out.print(" * ");
+        // }
+        // System.out.println();
+        // }
+
+        /****************************************************************** */
+        // Hollow Rectangular Pattern
+        // for (int i = 1; i <= n; i++) {
+        // for (int j = 1; j <= k; j++) {
+        // if (i == 1 || j == 1 || i == n || j == k) {
+        // System.out.print("*");
+        // } else {
+        // System.out.print(" ");
+        // }
+        // }
+        // System.out.println();
+        // }
+
+        /****************************************************************** */
+        // Inverted half piramid
+        int n = 5;
+        // for (int i = n; i > 0; i--) {
+        // for (int j = 0; j < i; j++) {
+        // System.out.print(" * ");
+        // }
+        // System.out.println();
+        // }
+
+        /****************************************************************** */
+        // half piramid
+        // for (int i = 0; i < n; i++) {
+        // for (int j = 0; j < n - i; j++) {
+        // System.out.print(" ");
+        // }
+        // for(int j = 0 ; j<i; j++){
+        // System.out.print("*");
+
+        // }
+        // System.out.println();
+        // }
+
+          /****************************************************************** */
+//  print numbers in piramid form
+        //   for(int i =1; i<=n; i++){
+        //       for(int j =1 ; j<=i; j++){
+        //           System.out.print(j );
+        //       }
+        //       System.out.println();
+        //   }
+
+          /****************************************************************** */
+    //  print numbers in Inverted piramid form
+    }
+}
