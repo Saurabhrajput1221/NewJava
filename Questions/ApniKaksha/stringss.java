e77df452b4ddb19891ec79e40d79382bc56d4541
saurabh rajput <saurabhkgf2609@gmail.com>
2022-06-12 14:54:31 +0530

    pr

---
+++ Questions/ApniKaksha/stringss.java
@@ -0,0 +1,20 @@
+package Questions.ApniKaksha;
+
+import java.util.*;
+
+public class stringss {
+    public static void main(String[] args) {
+        System.out.println("Stringssssssssss");
+        // Scanner sc = new Scanner(System.in);
+        // String name = sc.nextLine();
+        // System.out.println("name is " + name);
+
+        // String fname = "hello ";
+        // String lname = "word";
+        // String m = fname.concat(lname);
+        // System.out.println(m);
+        /********************************************************** */
+        // String Builder is same as String
+
+    }
+}
