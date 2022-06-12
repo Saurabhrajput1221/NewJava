007124b5c7747fdefdefe485b97a2aa5d7971310
saurabh rajput <saurabhkgf2609@gmail.com>
2022-06-12 13:20:30 +0530

    pr

--- Questions/AdvancePatterns.java
+++ Questions/AdvancePatterns.java
@@ -34,21 +34,74 @@ public class AdvancePatterns {
         // }
 
         /****************************************************************** */
-        // solid Pattern
+        // solid Rhombus Pattern
         // int n = 5;
         // for (int i = 1; i <= n; i++) {
-        //     int spaces = n - i;
-        //     for (int k = 0; k <= spaces; k++) {
-        //         System.out.print(" ");
-        //     }
-        //     for (int j = 1; j <= n; j++) {
-        //         System.out.print("*");
-        //     }
+        // int spaces = n - i;
+        // for (int k = 0; k <= spaces; k++) {
+        // System.out.print(" ");
+        // }
+        // for (int j = 1; j <= n; j++) {
+        // System.out.print("*");
+        // }
 
-        //     System.out.println();
+        // System.out.println();
         // }
         /****************************************************************** */
-    // Number pyramid
+        // Number pyramid
+        // int n = 5;
+        // for (int i = 1; i <= n; i++) {
+        // int spaces = n - i;
+        // for (int j = 0; j <= spaces; j++) {
+        // System.out.print(" ");
+        // }
+        // for (int j = 1; j <= i; j++) {
+        // System.out.print(i + " ");
+        // }
+        // System.out.println();
+        // }
+        /****************************************************************** */
+        // Palindromic Pattern
+        // int n = 5;
+        // for (int i = 1; i <= n; i++) {
+        // int spaces = n - i;
+        // for (int j = 1; j <= spaces; j++) {
+        // System.out.print(" ");
+        // }
+        // for (int j = i; j >= 1; j--) {
+        // System.out.print(j);
+
+        // }
+        // for (int j = 2; j <= i; j++) {
+        // System.out.print(j);
+        // }
+        // System.out.println();
+        // }
+        /****************************************************************** */
+        // Diamond Pattern
+        // int n = 4;
+        // for (int i = 1; i <= n; i++) {
+        // int spaces = n - i;
+        // for (int j = 1; j <= spaces; j++) {
+        // System.out.print(" ");
+        // }
+        // for (int j = 1; j <= 2 * i - 1; j++) {
+        // System.out.print("*");
+        // }
+
+        // System.out.println();
+        // }
+        // for (int i = n; i>=1; i--) {
+        // int spaces = n - i;
+        // for (int j = 1; j <= spaces; j++) {
+        // System.out.print(" ");
+        // }
+        // for (int j = 1; j <= 2 * i - 1; j++) {
+        // System.out.print("*");
+        // }
+
+        // System.out.println();
+        // }
 
     }
 }
