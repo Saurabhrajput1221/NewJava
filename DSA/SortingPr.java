7d784de7d349f646fc7f7e8540c7a2686c7f90c2
saurabh rajput <saurabhkgf2609@gmail.com>
2022-06-12 22:12:31 +0530

    pr

---
+++ DSA/SortingPr.java
@@ -0,0 +1,27 @@
+package DSA;
+
+import Questions.BubbleSortQue;
+
+public class SortingPr {
+    public static void main(String[] args) {
+        /**************************************************** */
+        // // Bubble Sort
+        // System.out.println("Bubble Sort");
+        // int arr[] = { 5, 4, 3, 6, 2, 1 };
+
+        // for (int i = 0; i < arr.length - 1; i++) {
+        //     for (int j = 0; j < arr.length - i - 1; j++) {
+
+        //         if (arr[j] > arr[j + 1]) {
+        //             int temp = arr[j];
+        //             arr[j] = arr[j + 1];
+        //             arr[j + 1] = temp;
+        //         }
+        //     }
+        // }
+        // for (int i = 0; i < arr.length; i++) {
+        //     System.out.print(arr[i] + " ");
+        // }
+         /**************************************************** */
+    }
+}
