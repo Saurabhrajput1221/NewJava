23a53a9610c9bb5cdde4673d0e4294e110f57bd6
saurabh rajput <saurabhkgf2609@gmail.com>
2022-06-12 22:12:40 +0530

    pr

--- Questions/BubbleSortQue.java
+++ Questions/BubbleSortQue.java
@@ -1,46 +1,46 @@
-// package Questions;
+package Questions;
 
 // import java.util.Scanner;
 
-// public class BubbleSortQue {
-//     public static void main(String[] args) {
-       
-//         // System.out.print("Enter length of integer ");
-//         // Scanner sc = new Scanner(System.in);
-//         // int n = sc.nextInt();
-//         // int arr[] = new int[n];
-//         // for(int i =0; i<n; i++){
-//         //   arr[i] = sc.nextInt();
-//         // System.out.print ( arr[i] + " ");
-//         // }
-//         Scanner sc = new Scanner(System.in);
-//         System.out.print("Enter length of integer ");
-//         int swap = 0;
-//         int n = sc.nextInt();
-//         int arr[] = new int[n];
-//         for(int i =0; i<n; i++){
-//           arr[i] = sc.nextInt();
-//         // System.out.print ("Enter length of integer " + arr[i]);
-//         }
-        
-//         for(int i=0; i<arr.length -1; i++){
-//           for(int j =0; j<arr.length-1-i; j++){
-//             if(arr[j]> arr[j+1]){
-//               int temp = arr[j];
-//               arr[j] = arr[j+1];
-//               arr[j+1] = temp;
-//             }
-//           }
-//           swap++;
-//         }
-//         System.out.print("Sorted Array list ");
-//         for(int i=0; i<arr.length; i++){
-//           System.out.print(arr[i]);
+public class BubbleSortQue {
+    public static void main(String[] args) {
 
-//         }
-//         System.out.println("Array is sorted in " + swap + " swaps.");
-//         System.out.println("First Element of Array is " + arr[0]);
-//         System.out.println("Last Element of Array is " + arr[arr.length-1] );
-        
-//     }
-// }
+        // // System.out.print("Enter length of integer ");
+        // // Scanner sc = new Scanner(System.in);
+        // // int n = sc.nextInt();
+        // // int arr[] = new int[n];
+        // // for(int i =0; i<n; i++){
+        // // arr[i] = sc.nextInt();
+        // // System.out.print ( arr[i] + " ");
+        // // }
+        // Scanner sc = new Scanner(System.in);
+        // System.out.print("Enter length of integer ");
+        // int swap = 0;
+        // int n = sc.nextInt();
+        // int arr[] = new int[n];
+        // for(int i =0; i<n; i++){
+        // arr[i] = sc.nextInt();
+        // // System.out.print ("Enter length of integer " + arr[i]);
+        // }
+
+        // for(int i=0; i<arr.length -1; i++){
+        // for(int j =0; j<arr.length-1-i; j++){
+        // if(arr[j]> arr[j+1]){
+        // int temp = arr[j];
+        // arr[j] = arr[j+1];
+        // arr[j+1] = temp;
+        // }
+        // }
+        // swap++;
+        // }
+        // System.out.print("Sorted Array list ");
+        // for(int i=0; i<arr.length; i++){
+        // System.out.print(arr[i]);
+
+        // }
+        // System.out.println("Array is sorted in " + swap + " swaps.");
+        // System.out.println("First Element of Array is " + arr[0]);
+        // System.out.println("Last Element of Array is " + arr[arr.length-1] );
+
+    }
+}
