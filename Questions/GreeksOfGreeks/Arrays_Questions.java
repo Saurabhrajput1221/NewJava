bf70cc8325c48afbc053b2fe526ac8d1dfd28e32
saurabh rajput <saurabhkgf2609@gmail.com>
2022-06-30 12:54:26 +0530

    1St Array Question

---
+++ Questions/GreeksOfGreeks/Arrays_Questions.java
@@ -0,0 +1,61 @@
+package Questions.GreeksOfGreeks;
+
+import java.util.Scanner;
+
+public class Arrays_Questions {
+
+    // Solved problem using function same on Geeks of Greeks
+    // public static void MinMax(int items[], int n) {
+    // int max = items[0];
+    // int min = items[0];
+
+    // for (int i = 1; i < items.length; i++) {
+    // // int max;
+    // if (max > items[i]) {
+    // max = items[i];
+    // } else if (min < items[i]) {
+    // min = items[i];
+    // }
+    // }
+    // System.out.println(max);
+    // System.out.println(min);
+    // }
+    /************************************************************************** */
+    public static void main(String args[]) {
+        System.out.println("hello");
+
+        /************************************************************************** */
+        // int items[] = {1000, 11, 445, 1, 330, 3000 };
+        // int n = items.length;
+        // MinMax(items, n);
+        /************************************************************************** */
+        // // // print Maximum and Minimum Element in an Array
+        // // Take user input
+
+        // // Scanner sc = new Scanner(System.in);
+        // // int n = sc.nextInt();
+        // // int items[] = new int[n];
+        // // for (int i = 0; i < items.length; i++) {
+        // // items[i] = sc.nextInt();
+        // // }
+        // // // for(int i =0 ;i<items.length; i++){
+        // // // // System.out.print(items[i]+ " ");
+        // // // }
+
+        // // // int items[] = { 5000, 11, 445, 6, 330, 3000 };
+        // // int min = items[0];
+        // // int max = items[0];
+
+        // // for (int i = 1; i < items.length; i++) {
+        // // if (min > items[i]) {
+        // // min = items[i];
+        // // } else if (max < items[i]) {
+        // // max = items[i];
+        // // }
+        // // }
+        // // System.out.println("Maximum Number from array is " + max);
+        // // System.out.println("Minimum Number from array is " + min);
+
+    }
+
+}
