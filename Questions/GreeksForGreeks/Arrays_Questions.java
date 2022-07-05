8ba65085f2b9c708e87e97d6ac99b1f66121d743
saurabh rajput <saurabhkgf2609@gmail.com>
2022-07-05 13:54:24 +0530

    Question number 7 and 8 solved

--- Questions/GreeksForGreeks/Arrays_Questions.java
+++ Questions/GreeksForGreeks/Arrays_Questions.java
@@ -1,7 +1,8 @@
 package Questions.GreeksForGreeks;
 
-import java.lang.reflect.Array;
-import java.util.Arrays;
+// import java.lang.reflect.Array;
+// import java.util.Arrays;
+// import java.util.Vector;
 
 // import java.net.Socket;
 // import java.util.Arrays;
@@ -135,66 +136,72 @@ public class Arrays_Questions {
     // }
     // }
     // }
+    /**
+     */
+    /************************************************************************** */
+    // Next premutation // confusing to me
+    // public static void permutation(int[] nums) {
+    // int start = nums.length - 2;
+    // System.out.println("Start index " + start);
+    // int end = 0;
+    // System.out.println("end index " + nums[end]);
+    // while (start >= 0 && nums[start] >= nums[start + 1])
+    // start--;
+    // if (start >= 0) {
+    // end = nums.length - 1;
+    // while (nums[end] <= nums[start])
+    // end--;
+    // swap(nums, start, end);
+    // }
+    // reverse(nums, start + 1, nums.length - 1);
+
+    // for (int i = 0; i < nums.length; i++) {
+    // System.out.print(nums[i] + " ");
+    // }
+
+    // }
+
+    // public static void swap(int nums[], int start, int end) {
+    // int temp = 0;
+    // temp = nums[start];
+    // nums[start] = nums[end];
+    // nums[end] = temp;
+    // }
+
+    // public static void reverse(int nums[], int start, int end) {
+    // while (start < end) {
+    // swap(nums, start++, end--);
+    // }
+    // }
+
     /************************************************************************** */
 
-    // Search an Element in a Sorted and Pivoted Array
-    public static int pivotedBinarySearch(int arr[], int n, int key){
-        int pivot = findPivot(arr, 0 ,n-1);
-        System.out.println("pivot "+pivot);
-        if (pivot == -1)
-        return binarySearch(arr, 0, n - 1, key);
+    public static int MaxProfit(int price[]) {
+        int s = Integer.MAX_VALUE;
+        int Op = 0;
+       int Pot = 0;
 
-    // If we found a pivot, then first
-    // compare with pivot and then
-    // search in two subarrays around pivot
-    if (arr[pivot] == key)
-        return pivot;
-    if (arr[0] <= key)
-        return binarySearch(arr, 0, pivot - 1, key);
-    return binarySearch(arr, pivot + 1, n - 1, key);
-}
-
-
-    
-    static int findPivot(int arr[], int low, int high)
-    {
-        // base cases
-        if (high < low)
-            return -1;
-        if (high == low)
-            return low;
- 
-        /* low + (high - low)/2; */
-        int mid = (low + high) / 2;
-        if (mid < high && arr[mid] > arr[mid + 1])
-            return mid;
-        if (mid > low && arr[mid] < arr[mid - 1])
-            return (mid - 1);
-        if (arr[low] >= arr[mid])
-            return findPivot(arr, low, mid - 1);
-        return findPivot(arr, mid + 1, high);
-    }
-    static int binarySearch(int arr[], int low, int high, int key)
-    {
-        if (high < low)
-            return -1;
- 
-        /* low + (high - low)/2; */
-        int mid = (low + high) / 2;
-        if (key == arr[mid])
-            return mid;
-        if (key > arr[mid])
-            return binarySearch(arr, (mid + 1), high, key);
-        return binarySearch(arr, low, (mid - 1), key);
+       for(int i =0 ;i<price.length ; i++){
+           if(price[i]< s){
+               s = price[i];
+           }
+            Pot = price[i] - Op;
+            if(Op<Pot){
+                Op = Pot;
+            }
+        }
+        return Op;
     }
 
     public static void main(String args[]) {
         System.out.println("hello");
-        int arr1[] = { 5, 6, 7, 8, 9, 10, 1, 2, 3 };
-        int n = arr1.length;
-        int key = 3;
-        System.out.println("Index of the element is : "
-                           + pivotedBinarySearch(arr1, n, key));
+       int price[] = {7,6,4,3,1};
+       System.out.println(MaxProfit(price));
+        /************************************************************************** */
+        // Next permutation problem
+        // int nums[] = { 1,1,5 };
+        // permutation(nums);
+
         /************************************************************************** */
         // int arr[] = {5, 6,7,8,9,10,1,2,3};
         // int key = 10;
