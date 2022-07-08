5a130c371a5bb90adb9231a2e6f30f83c93134ea
saurabh rajput <saurabhkgf2609@gmail.com>
2022-07-08 12:14:58 +0530

    Questjon No 10 (Kth-Largest Element in an Array)

---
+++ Questions/GreeksForGreeks/ArrayHardQuestions.java
@@ -0,0 +1,31 @@
+package Questions.GreeksForGreeks;
+
+import java.util.Arrays;
+
+public class ArrayHardQuestions {
+    /****************************************************************** */
+    // Kth Largest Element in an Array
+    // public static int findLargestElm(int nums[], int k) {
+    // Arrays.sort(nums);
+    // int i = nums.length;
+    // // System.out.println("length of array " + i );
+    // // for(int j=0;j<i;j++){
+    // // System.out.print(nums[j] + " ");
+    // // }
+    // return nums[i - k];
+    // }
+
+    /****************************************************************** */
+
+    public static void main(String[] args) {
+        // Kth Largest Element in an Array
+        System.out.println("Array");
+        /****************************************************************** */
+        // int nums[] = { 3, 2, 1, 5, 6, 4 };
+        // int k = 4;
+        // System.out.println(" ans " + findLargestElm(nums, k));
+
+        /****************************************************************** */
+
+    }
+}
--- Questions/GreeksForGreeks/Arrays_Questions.java
+++ Questions/GreeksForGreeks/Arrays_Questions.java
@@ -219,37 +219,10 @@ public class Arrays_Questions {
     // }
     /************************************************************************** */
 
-    public static int trap(int[] height) {
-        int n = height.length;
-        int res = 0;
-        int left[] = new int[n];
-        int right[] = new int[n];
-
-        int max = height[0];
-        for (int i = 0; i < n; i++) {
-            max = Math.max(max, height[i]);
-            left[i] = max;
-        }
-
-        max = height[n - 1];
-        for (int i = n - 1; i >= 0; i--) {
-            max = Math.max(max, height[i]);
-            right[i] = max;
-        }
-
-        for (int i = 0; i < n; i++) {
-            res += Math.min(left[i], right[i]) - height[i];
-        }
-
-        return res;
-    }
 
     public static void main(String args[]) {
         System.out.println("hello");
-        // int heigth[] = {4,2,0,3,2,5};
-        int heigth[] = { 0, 1, 0, 2, 1, 0, 1, 3, 2, 1, 2, 1 };
-
-        System.out.println(trap(heigth));
+       
 
         /************************************************************************** */
         // // Repeat and Missing Number Array
