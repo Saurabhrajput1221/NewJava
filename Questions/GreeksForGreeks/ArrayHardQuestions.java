f7b7732d73b7ca043572e42ce4ea7babcfd37a00
saurabh rajput <saurabhkgf2609@gmail.com>
2022-07-10 14:22:35 +0530

    a

--- Questions/GreeksForGreeks/ArrayHardQuestions.java
+++ Questions/GreeksForGreeks/ArrayHardQuestions.java
@@ -94,66 +94,44 @@ public class ArrayHardQuestions {
     // return result;
     // }
     /****************************************************************** */
-
+    // Find Minimum in Rotated Sorted Array
     // public static int findMin(int arr[]) {
-    //     int n = arr.length;
-    //     int start = 0;
-    //     int end = n - 1;
-    //     if (arr[start] <= arr[end]) {
-    //         return arr[start];
-    //     }
-    //     while (start <= end) {
-    //         int mid = start + (end - start) / 2;
-
-    //         if (arr[mid] > arr[mid + 1]) {
-    //             return arr[mid + 1];
-    //         } else if (arr[mid] < arr[mid - 1]) {
-    //             return arr[mid];
-    //         } else if (arr[start] < arr[mid]) {
-    //             start = mid + 1;
-    //         } else if (arr[mid] < arr[end]) {
-    //             end = mid - 1;
-    //         }
-    //     }
-    //     return -1;
+    // int l = 0;
+    // int r = arr.length - 1;
+    // if (arr.length == 1) {
+    // return arr[0];
     // }
+    // if (arr[l] <= arr[r]) {
+    // return arr[l];
+    // }
+    // while (l <= r) {
+    // int mid = l + (r - l) / 2;
+    // if (arr[mid] > arr[mid + 1]) {
+    // return arr[mid + 1];
+    // } else if (arr[mid] < arr[mid - 1]) {
+    // return arr[mid];
+    // } else if (arr[l] < arr[mid]) {
+    // l = mid + 1;
 
-    public static int findMin(int arr[]) {
-        int l = 0;
-        int r = arr.length - 1;
-        if (arr.length == 1) {
-            return arr[0];
-        }
-        if (arr[l] <= arr[r]) {
-            return arr[l];
-        }
-        while(l<= r){
-            int mid = l +(r -l)/2;
-             if(arr[mid]> arr[mid+1]){
-                 return arr[mid+1];
-             }else if(arr[mid]< arr[mid-1]){
-                 return arr[mid];
-             }else if(arr[l]< arr[mid]){
-                 l = mid+1;
+    // } else if (arr[mid] < arr[r]) {
+    // r = mid - 1;
+    // }
+    // }
+    // return -1;
+    // }
+    /****************************************************************** */
 
-             }
-             else if(arr[mid]<arr[r]){
-                 r = mid -1;
-             }
-        }
-        return -1;
-    }
-
+    
     public static void main(String[] args) {
         System.out.println("Array");
+
+        /****************************************************************** */
+        // Find Minimum in Rotated Sorted Array
         // int arr[] = { 5, 6, 7, 8, 1, 2 };
         // int arr[] = { 4, 5, 6, 7, 8, 0, 1, 3 };
-        int arr[] = { 11, 13, 15, 17 };
-        System.out.println(findMin(arr));
+        // int arr[] = { 11, 13, 15, 17 };
+        // System.out.println(findMin(arr));
 
-        // for(int i=0 ; i<arr.length; i++){
-        // System.out.println(arr[i]);
-        // }
         /****************************************************************** */
         // int arr[] = { 2, 3, -2, 4 };
         // int arr[] = {0,2};
