0689949bdef3ce1d5fcebe1026d556f1acaa5efd
saurabh rajput <saurabhkgf2609@gmail.com>
2022-07-12 09:59:40 +0530

    valid Palindrome

--- Questions/GreeksForGreeks/String_Questions.java
+++ Questions/GreeksForGreeks/String_Questions.java
@@ -1,31 +1,37 @@
 package Questions.GreeksForGreeks;
 
-import javax.print.FlavorException;
-
 public class String_Questions {
-
-    public static boolean Ispalindrome(String s) {
-        s = s.toLowerCase();
-        StringBuilder b = new StringBuilder();
-        for (int i = 0; i < s.length(); i++) {
-            if (Character.isDigit(s.charAt(i)) || Character.isLetter(s.charAt(i))) {
-                b.append(s.charAt(i));
-                // System.out.println(b);
-            }
-        }
-        int n = b.length() - 1;
-        for (int i = 0; i < b.length()/2; i++) {
-            if (b.charAt(i) != b.charAt(n-i)) {
-                return false;
-            }
-        }
-        return true;
-    }
+    // valid Palindrome
+    // public static boolean Ispalindrome(String s) {
+    // s = s.toLowerCase();
+    // StringBuilder b = new StringBuilder();
+    // for (int i = 0; i < s.length(); i++) {
+    // if (Character.isLetter(s.charAt(i))) {
+    // b.append(s.charAt(i));
+    // // System.out.println(b);
+    // }
+    // }
+    // int n = b.length() - 1;
+    // for (int i = 0; i < b.length() / 2; i++) {
+    // if (b.charAt(i) != b.charAt(n - i)) {
+    // System.out.println("string is not a palindrome ");
+    // return false;
+    // }
+    // }
+    // System.out.println("string is a palindrome ");
+    // return true;
+    // }
+    /*************************************************************************** */
+     
 
     public static void main(String[] args) {
         System.out.println("String...........");
-        String s = "A Man, a plan , a canal: panama";
-       System.out.println(Ispalindrome(s));
-  
+
+
+        /*************************************************************************** */
+        // valid Palindrome
+        // String s = "A Man, a plan , a canal: panama";
+        // System.out.println(Ispalindrome(s));
+
     }
 }
