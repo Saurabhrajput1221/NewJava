8b9480e34146ba6080fa6f86f525a8ae10412b36
saurabh rajput <saurabhkgf2609@gmail.com>
2022-07-13 13:50:08 +0530

    Convert a Sentence into its Equivalent Mobile Numeric Keypad Sequence

--- Questions/GreeksForGreeks/String_Questions.java
+++ Questions/GreeksForGreeks/String_Questions.java
@@ -1,5 +1,7 @@
 package Questions.GreeksForGreeks;
 
+import java.net.Socket;
+
 // import java.rmi.dgc.Lease;
 // import java.util.Stack;
 
@@ -101,29 +103,57 @@ public class String_Questions {
     // }
     /*************************************************************************** */
     // Longest Common Prefix Difficult to Understand.
-    public static String Prefix(String[] s) {
-        String Result = s[0];
-        // String[] s = {"flower","flow","flight"};
-        // System.out.println(Result);
-        if (s.length == 0) {
-            return "";
+    // public static String Prefix(String[] s) {
+    // String Result = s[0];
+    // // String[] s = {"flower","flow","flight"};
+    // // System.out.println(Result);
+    // if (s.length == 0) {
+    // return "";
+    // }
+    // for (int i = 1; i < s.length; i++) {
+    // // System.out.println(s[i].indexOf(Result) != 0);
+    // while (s[i].indexOf(Result) != 0) {
+    // Result = Result.substring(0, Result.length() - 1);
+    // }
+    // }
+    // return Result;
+    // }
+    /*************************************************************************** */
+    // Convert a Sentence into its Equivalent Mobile Numeric Keypad Sequence
+public static String MobileKeypad(String[] s , String input){
+    String output = "";
+    for(int i = 0 ;i<input.length();i++){
+        if(input.charAt(i) == ' '){
+            output += " ";
+        }else{
+            output += s[input.charAt(i) - 'A'];
         }
-        for (int i = 1; i < s.length; i++) {
-            // System.out.println(s[i].indexOf(Result) != 0);
-            while (s[i].indexOf(Result) != 0) {
-                Result = Result.substring(0, Result.length() - 1);
-            }
-        }
-        return Result;
+
     }
+    return output;
+
+}
 
     public static void main(String[] args) {
         System.out.println("String...........");
+                /*************************************************************************** */
+        // Convert a Sentence into its Equivalent Mobile Numeric Keypad Sequence
+        String s[] = { "2", "22", "222",
+                "3", "33", "333",
+                "4", "44", "444",
+                "5", "55", "555",
+                "6", "66", "666",
+                "7", "77", "777", "7777",
+                "8", "88", "888",
+                "9", "99", "999", "9999" };
+        String input = "AB";
+        System.out.println(MobileKeypad(s, input));
+        /*************************************************************************** */
 
         // Longest Common Prefix
-        String[] s = { "flower", "flow", "flight" };
-        System.out.println(Prefix(s));
-        String s1 = "hello";
+        // String[] s = { "flower", "flow", "flight" };
+        // System.out.println(Prefix(s));
+        // String s1 = "hello";
         // System.out.println( "Ans of indexs " + s1.indexOf('o'));
         // System.out.println(s.(3,2));
 
