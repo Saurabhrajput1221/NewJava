03870a705a84a256a01359466846ccf441b41175
saurabh rajput <saurabhkgf2609@gmail.com>
2022-07-16 12:36:49 +0530

    Question 1

---
+++ Questions/LeetCode/SearchingAndSorting.java
@@ -0,0 +1,32 @@
+package Questions.LeetCode;
+
+import java.util.Arrays;
+import java.util.Collections;
+
+public class SearchingAndSorting {
+    // Permute Two Arrays such that Sum of Every Pair is Greater or Equal to K
+    // public static String IsPossible(Integer[] a, int b[], int k){
+    // Arrays.sort(a, Collections.reverseOrder());
+    // // System.out.print( Arrays.sort(a, Collections.reverseOrder()));
+
+    // // Sort the array b[] in increasing order.
+    // Arrays.sort(b);
+    // for(int i =0;i<a.length;i++){
+    // if(a[i] + b[i] <= k){
+    // return "NO";
+    // }
+    // }
+    // return "YES";
+    // }
+    /**************************************************************************/
+    public static void main(String[] args) {
+        System.out.println("Searching And Sorting...");
+        /**************************************************************************/
+        // Permute Two Arrays such that Sum of Every Pair is Greater or Equal to K
+        // Integer a[] = { 1, 2, 2, 1 };
+        // int b[] = { 3, 3, 3, 4 };
+        // int k = 10;
+        // System.out.println(IsPossible(a, b, k));
+
+    }
+}
--- Questions/LeetCode/StringHardQuestions.java
+++ Questions/LeetCode/StringHardQuestions.java
@@ -26,69 +26,38 @@ public class StringHardQuestions {
     // return max;
     // }
     /************************************************************************** */
-// To much confusing.
+    // Strings Longest Repeating Character Replacement. To much confusing.
     // public static int CharReplace(String s, int n) {
-    //     int[] fre = new int[26];
-    //     int start = 0;
-    //     int freqlength = 0;
-    //     int Longest = 0;
+    // int[] fre = new int[26];
+    // int start = 0;
+    // int freqlength = 0;
+    // int Longest = 0;
 
-    //     for (int i = 0; i < s.length(); i++) {
-    //         fre[s.charAt(i) - 'A']++;
-    //         System.out.println("Freq " + fre[s.charAt(i) - 'A']);
+    // for (int i = 0; i < s.length(); i++) {
+    // fre[s.charAt(i) - 'A']++;
+    // System.out.println("Freq " + fre[s.charAt(i) - 'A']);
 
-    //         freqlength = Math.max(freqlength, fre[s.charAt(i) - 'A']);
-    //         System.out.println("Length of freqlength " + freqlength);
-    //         int maxwindow = i - start + 1;
-    //         System.out.println("maxWindow " + maxwindow);
+    // freqlength = Math.max(freqlength, fre[s.charAt(i) - 'A']);
+    // System.out.println("Length of freqlength " + freqlength);
+    // int maxwindow = i - start + 1;
+    // System.out.println("maxWindow " + maxwindow);
 
-    //         if (maxwindow - freqlength > n) {
-    //             fre[s.charAt(start) - 'A']--;
-    //             start++;
-    //         }
-    //         Longest = Math.max(Longest, i - start + 1);
-    //         System.out.println("Longest " + Longest);
-
-    //     }
-    //     return Longest;
+    // if (maxwindow - freqlength > n) {
+    // fre[s.charAt(start) - 'A']--;
+    // start++;
+    // }
+    // Longest = Math.max(Longest, i - start + 1);
+    // System.out.println("Longest " + Longest);
+    // }
+    // return Longest;
     // }
     /************************************************************************** */
-
-
-// String ans = "";
-        
-// for(int i=0; i<s.length(); i++){
-//     int low = i, high = i;
-//     while(low >= 0 && high < s.length() && s.charAt(low) == s.charAt(high)){
-//         if(ans.length() < high-low+1){
-//             ans = s.substring(low, high+1);
-//         }
-//         low--;
-//         high++;
-//     }
-//     low = i;
-//     high = i+1;
-//     while(low >= 0 && high < s.length() && s.charAt(low) == s.charAt(high)){
-//         if(ans.length() < high-low+1){
-//             ans = s.substring(low, high+1);
-//         }
-//         low--;
-//         high++;
-//     }
-// }
-// return ans;
-
-
-    
-
-
-
 
     public static void main(String[] args) {
         System.out.println("String Hard Questions ");
 
-
         /************************************************************************** */
+        // Strings Longest Repeating Character Replacement
         // String s = "ABAB";
         // int n = 2;
         // System.out.println(CharReplace(s, n));
