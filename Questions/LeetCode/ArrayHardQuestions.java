b58c58c6c3862c27f57072402266ba3e06bce5fc
saurabh rajput <saurabhkgf2609@gmail.com>
2022-07-17 23:22:21 +0530

    practice

--- Questions/LeetCode/ArrayHardQuestions.java
+++ Questions/LeetCode/ArrayHardQuestions.java
@@ -1,5 +1,10 @@
 package Questions.LeetCode;
 
+import java.rmi.server.RemoteCall;
+import java.util.Arrays;
+
+import javax.lang.model.element.Element;
+
 // import java.lang.annotation.Target;
 // import java.util.ArrayList;
 // import java.util.Arrays;
@@ -16,7 +21,7 @@ package Questions.LeetCode;
 // import Questions.ApniKaksha.str;
 
 public class ArrayHardQuestions {
-    /****************************************************************** */
+    /*******************************************************************/
     // Kth Largest Element in an Array
     // public static int findLargestElm(int nums[], int k) {
     // Arrays.sort(nums);
@@ -28,7 +33,7 @@ public class ArrayHardQuestions {
     // return nums[i - k];
     // }
 
-    /****************************************************************** */
+    /*******************************************************************/
     // Trapping Rain Water
     // public static int trap(int height[]) {
     // int Rmax = 0;
@@ -36,13 +41,15 @@ public class ArrayHardQuestions {
     // int result = 0;
     // int i = 0;
     // int j = height.length - 1;
-
+    // // 4, 2, 0, 3, 2, 5
     // while (i <= j) {
     // if (height[i] <= height[j]) {
+
     // if (Lmax < height[i]) {
     // Lmax = height[i];
     // } else {
-    // result += Lmax - height[i];
+    // result = result + Lmax - height[i];
+    // // System.out.println("this is result " + result);
     // }
     // i++;
     // } else {
@@ -50,17 +57,22 @@ public class ArrayHardQuestions {
     // Rmax = height[j];
     // } else {
     // result += Rmax - height[j];
+    // System.out.println("this is result " + result);
     // }
     // j--;
     // }
     // }
     // return result;
     // }
-    /****************************************************************** */
-    // Product of Array Except Self
+
+    /*******************************************************************/
+    // Product of Array Except Self( need to sol again)
     // public static int productExceptSelf(int arr[]) {
     // int curr = 1;
     // int n = arr.length - 1;
+
+    // 1, 2, 3, 4
+
     // int ans[] = new int[n];
     // for (int i = 0; i < n; i++) {
     // ans[i] = curr;
@@ -77,7 +89,7 @@ public class ArrayHardQuestions {
     // }
     // return curr;
     // }
-    /****************************************************************** */
+    /*******************************************************************/
 
     // public static int maxProduct(int arr[]) {
     // int result = Integer.MIN_VALUE;
@@ -101,7 +113,8 @@ public class ArrayHardQuestions {
     // }
     // return result;
     // }
-    /****************************************************************** */
+
+    /*******************************************************************/
     // Find Minimum in Rotated Sorted Array
     // public static int findMin(int arr[]) {
     // int l = 0;
@@ -128,7 +141,7 @@ public class ArrayHardQuestions {
     // return -1;
     // }
 
-    /****************************************************************** */
+    /*******************************************************************/
     // 3 Sum ( understood but confusing)
     // public static List<List<Integer>> threesum(int arr[]) {
     // // public static int threesum(int arr[]) {
@@ -157,7 +170,7 @@ public class ArrayHardQuestions {
     // }
     // return new ArrayList(res);
     // }
-    /****************************************************************** */
+    /*******************************************************************/
     // Container With Most Water
     // public static int MaxWater(int arr[]) {
     // int start = 0;
@@ -176,16 +189,14 @@ public class ArrayHardQuestions {
     // }
     // return MaxWater;
     // }
-    // /****************************************************************** */
 
-
+    // /*******************************************************************/
 
     public static void main(String[] args) {
         System.out.println("Array");
         // int arr[] = { 11, 15, 6, 7, 9, 10 };
-      
 
-        /****************************************************************** */
+        /*******************************************************************/
         // Container With Most Water
         // int arr[] = {1,8,6,2,5,4,8,3,7};
         // int arr[] = { 6, 4, 2, 5, 4, 6, 1, 3, 5 };
@@ -193,39 +204,39 @@ public class ArrayHardQuestions {
         // int arr[] = {1,7};
         // int arr[] = { 1, 8, 6, 2, 5, 4, 8, 3, 7 };
         // System.out.println(MaxWater(arr));
-        /****************************************************************** */
+        /*******************************************************************/
         // 3 Sum
         // int arr[] = { -1,0,1,2,-1,-4};
         // System.out.println(threesum(arr));
-        /****************************************************************** */
+        /*******************************************************************/
         // Find Minimum in Rotated Sorted Array
         // int arr[] = { 5, 6, 7, 8, 1, 2 };
         // int arr[] = { 4, 5, 6, 7, 8, 0, 1, 3 };
         // int arr[] = { 11, 13, 15, 17 };
         // System.out.println(findMin(arr));
 
-        /****************************************************************** */
-        // int arr[] = { 2, 3, -2, 4 };
+        /*******************************************************************/
+        // int arr[] = { 2, 3, -2, 4,3 };
         // int arr[] = {0,2};
         // int arr[] = { -2, 0, -1 };
         // System.out.println(maxProduct(arr));
-        /****************************************************************** */
+        /*******************************************************************/
         // Product of Array Except Self
         // int arr[] = { 1, 2, 3, 4 };
         // productExceptSelf(arr);
 
-        /****************************************************************** */
-        // Trapping Rain Water
+        /*******************************************************************/
+        // // Trapping Rain Water
         // int height[] = { 4, 2, 0, 3, 2, 5 };
-        // // System.out.println(height.length-1 + " length of hi.");
+        // // // System.out.println(height.length-1 + " length of hi.");
         // // int height[] = { 0, 1, 0, 2, 1, 0, 1, 3, 2, 1, 2, 1 };
         // System.out.println(trap(height));
-        /****************************************************************** */
+        // /*******************************************************************/
         // int nums[] = { 3, 2, 1, 5, 6, 4 };
         // int k = 4;
         // System.out.println("ans " + findLargestElm(nums, k));
 
-        /****************************************************************** */
+        /*******************************************************************/
 
     }
 }
