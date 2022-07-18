58ecaec0db9e7c346b75d6de380a5583747ec15b
saurabh rajput <saurabhkgf2609@gmail.com>
2022-07-18 13:11:11 +0530

    find common elements three sorted arrays

--- Questions/LeetCode/SearchingAndSorting.java
+++ Questions/LeetCode/SearchingAndSorting.java
@@ -1,5 +1,9 @@
 package Questions.LeetCode;
 
+import java.util.ArrayList;
+
+import AdvanceJava.Arraylist;
+
 // import java.util.Arrays;
 // import java.util.Collections;
 
@@ -18,9 +22,6 @@ public class SearchingAndSorting {
     // }
     // return "YES";
     // }
-    public static String Ispossible(Integer []a, int b[], int k){
-        
-    }
 
     /**************************************************************************/
     // counting sort (Too much confusing)
@@ -67,9 +68,36 @@ public class SearchingAndSorting {
     // System.out.println(arr[i] + " ");
     // }
     // }
+    /**************************************************************************/
+    // find common elements three sorted arrays
+    // public static ArrayList<Integer> commanEle(int arr1[], int arr2[], int arr3[]) {
+    // int i = 0, j = 0, k = 0;
+    // while (i < arr1.length && j < arr2.length && k < arr3.length) {
 
+    // if (arr1[i] == arr2[j] && arr2[j] == arr3[k]) {
+    // System.out.println("arr1[i] is " + arr1[i] + " arr2[j] is "+ arr2[j] + "arr3[k] is " + arr3[k]);
+    // i++;
+    // j++;
+    // k++;
+    // } else if (arr1[i] < arr2[j]) {
+    // i++;
+    // } else if (arr2[j] < arr3[k]) {
+    // j++;
+    // } else {
+    // k++;
+    // }
+    // }
+    // return null;
+    // }
+    /**************************************************************************/
     public static void main(String[] args) {
         System.out.println("Searching And Sorting...");
+        /**************************************************************************/
+        // find common elements three sorted arrays
+        // int arr1[] = { 1, 5, 10, 20, 40, 80 };
+        // int arr2[] = { 6, 7, 20, 80, 100 };
+        // int arr3[] = { 3, 4, 15, 20, 30, 70, 80, 120 };
+        // commanEle(arr1, arr2, arr3);
         /**************************************************************************/
         // counting sort
         // int arr[] = { 1, 2, 3, 4, 2, 5, 8 };
