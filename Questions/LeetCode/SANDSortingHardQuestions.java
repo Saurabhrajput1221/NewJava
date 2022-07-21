06d2e76d80b8e485a3d1ca354c197fada0e3939b
saurabh rajput <saurabhkgf2609@gmail.com>
2022-07-21 13:22:16 +0530

    Maximum Sum Subsequence with no adjacent elements

--- Questions/LeetCode/SANDSortingHardQuestions.java
+++ Questions/LeetCode/SANDSortingHardQuestions.java
@@ -25,29 +25,48 @@ public class SANDSortingHardQuestions {
     // }
     /***********************************************************************/
     // count triplets with sum smaller that a given value
-    public static int countTriplets(int arr[], int sum) {
-        int ans = 0;
-        for (int i = 0; i < arr.length; i++) {
-            int j = i + 1;
-            int k = arr.length - 1;
-            while (j < k) {
-                if (arr[i] + arr[j] + arr[k] >= sum) {
-                    k--;
-                } else {
-                    ans = ans + (k - j);
-                    j++;
-                }
-            }
-        }
-        return ans;
+    // public static int countTriplets(int arr[], int sum) {
+    // int ans = 0;
+    // Arrays.sort(arr);
+    // // 1,3,4,5,7
+    // for (int i = 0; i < arr.length; i++) {
+    // int j = i + 1;
+    // int k = arr.length - 1;
+    // while (j < k) {
+    // if (arr[i] + arr[j] + arr[k] >= sum) {
+    // k--;
+    // } else {
+    // ans = ans + (k - j);
+    // j++;
+    // }
+    // }
+    // }
+    // return ans;
+    // }
+    /***********************************************************************/
+    // Maximum Sum Subsequence with no adjacent elements
+    public static int findMaxSum(int arr[]) {
+    int icld = arr[0];
+    int excl = 0;
+    int excl_new;
+    for (int i = 1; i < arr.length; i++) {
+    excl_new = Math.max(icld, excl);
+    icld = excl + arr[i];
+    excl = excl_new;
+    }
+    return Math.max(icld, excl);
     }
 
     public static void main(String[] args) {
         System.out.println("Searching and Sorting Hard Question...");
+        // Maximum Sum Subsequence with no adjacent elements
+        int arr[] = { 5, 5, 10, 100, 10, 5 };
+        System.out.println(findMaxSum(arr));
+        /***********************************************************************/
         // count triplets with sum smaller that a given value
-        int arr[] = { 5, 1, 3, 4, 7 };
-        int sum = 12;
-        System.out.println(countTriplets(arr, sum));
+        // int arr[] = { 5, 1, 3, 4, 7 };
+        // int sum = 12;
+        // System.out.println(countTriplets(arr, sum));
         /***********************************************************************/
         // majority element
         // int arr[] = { 3, 3, 4, 2, 4, 4, 2, 4, 4 };
